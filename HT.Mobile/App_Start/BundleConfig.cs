a26615a03519a203261cd74dbe158859b5801221
lijunboy <lijunboy168@126.com>
2018-05-06 17:42:21 +0800

    移除Api

---
+++ HT.Mobile/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace HT.Mobile
+{
+    public class BundleConfig
+    {
+        // 有关捆绑的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
+            // 生产准备就绪，请使用 https://modernizr.com 上的生成工具仅选择所需的测试。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/baseCss").Include(
+                      "~/Scripts/swiper/swiper.min.css",
+                      "~/Scripts/layer/theme/default/layer.css",
+                      "~/Content/base.css",
+                      "~/Content/style.css"));
+
+            bundles.Add(new ScriptBundle("~/baseJs").Include(
+                      "~/Scripts/jquery-2.1.4.min.js",
+                      "~/Scripts/swiper/swiper.min.js",
+                      "~/Scripts/layer/layer.js",
+                      "~/Scripts/common.js"));
+        }
+    }
+}
---
+++ HT.Mobile/Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HT.Mobile.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //货源列表
+        public ActionResult GoodsSource()
+        {
+            return View();
+        }
+        //货源详情
+        public ActionResult GoodsSourceDetails()
+        {
+            return View();
+        }
+
+    }
+}
