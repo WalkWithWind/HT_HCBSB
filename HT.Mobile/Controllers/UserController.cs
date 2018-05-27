d38d10ec5c461b84e108d82c0cee0c8eaf3a3acc
xiahaitao <1021040213@qq.com>
2018-05-27 18:20:29 +0800

    我的团队

--- HT.Mobile/Controllers/UserController.cs
+++ HT.Mobile/Controllers/UserController.cs
@@ -209,6 +209,61 @@ namespace HT.Mobile.Controllers
             }
             return JsonResult(APIErrCode.CheckCodeErr, "提交失败");
         }
+        /// <summary>
+        /// 我的团队
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="rows"></param>
+        /// <returns></returns>
+        public ActionResult TeamList(int page,int rows)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                int total = 0;
+                decimal totalMoney = 0;
+                int totalPeopleNum = 0;
+                List<ht_commission> teamlist = BLLDistribution.GetCommussionList(page, rows, BLLUser.GetUserId(), out total,out totalMoney,out totalPeopleNum);
+                apiResp.result = new
+                {
+                    list = teamlist,
+                    total = total,
+                    total_money = totalMoney,
+                    total_people_num = totalPeopleNum
+                };
+                apiResp.msg = "查询完成";
+                apiResp.status = true;
+                return Json(apiResp);
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// 团队详情
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="rows"></param>
+        /// <param name="parentid"></param>
+        /// <returns></returns>
+        public ActionResult TeamChildList(int page,int rows,int parentid)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                int total = 0;
+                var list = BLLDistribution.GetCommussionByChild(page, rows, parentid, out total); apiResp.result = new
+                {
+                    list = list,
+                    total = total
+                };
+                apiResp.msg = "查询完成";
+                apiResp.status = true;
+                return Json(apiResp);
+            }
+            return View();
+        }
+
+
+
+
         #endregion 接口
     }
 }
